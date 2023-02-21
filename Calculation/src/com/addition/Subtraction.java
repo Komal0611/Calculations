952a0899e419294d02d1fe89a18b5db75b3f9c59
Arpitha <Arpitha@DESKTOP-J390HKU>
2023-02-21 10:15:22 +0530

    subtraction class added

---
+++ Calculation/src/com/addition/Subtraction.java
@@ -0,0 +1,13 @@
+package com.addition;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+
+		int a=12;
+		int b=35;
+		int result =b-a;
+		System.out.println(result);
+	}
+
+}
